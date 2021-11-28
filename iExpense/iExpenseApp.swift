b1b517af870e33785936fa7a3a230ce21afff68e
shivgirja <amitsrivastava115@yahoo.com>
2021-11-28 14:38:09 +0530

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Amit Shrivastava on 28/11/21.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
